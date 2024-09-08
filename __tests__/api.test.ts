3631ca32bdeb83349da8aef169474bdeae667292
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-09-08 13:07:46 +0000

    Fix tests

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -7,11 +7,14 @@ jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-it('fetches successfully data from an API', async () => {
-    const data = { data: { userId: 1, id: 1, title: 'mock title' } };
-    mockedAxios.get.mockResolvedValue(data);
+describe('api', () => {
+    it('fetches successfully data from an API', async () => {
+        const data = { data: { userId: 1, id: 1, title: 'mock title' } };
+        mockedAxios.get.mockResolvedValue(data);
 
-    const result = await fetchData('https://jsonplaceholder.typicode.com/todos/1');
-    expect(result).toEqual(data.data);
-    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+        const result = await fetchData('https://jsonplaceholder.typicode.com/todos/1');
+        expect(result).toEqual(data.data);
+        expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    });
 });
+
--- __tests__/sum.test.ts
+++ __tests__/sum.test.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@jest/globals';
 import { sum } from '../src/sum';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('sum', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
 });
