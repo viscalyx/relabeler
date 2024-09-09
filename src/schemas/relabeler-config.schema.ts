2fa261c81d83df03c15f6997329a107fb4b5ae0c
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-09-09 20:51:26 +0200

    fix lint errors

--- src/schemas/relabeler-config.schema.ts
+++ src/schemas/relabeler-config.schema.ts
@@ -1,134 +1,134 @@
 export const schema = {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "type": "object",
-    "properties": {
-        "pulls": {
-            "type": "object",
-            "properties": {
-                "labels": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "label": {
-                                "type": "object",
-                                "properties": {
-                                    "name": {
-                                        "type": "string"
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    'type': 'object',
+    'properties': {
+        'pulls': {
+            'type': 'object',
+            'properties': {
+                'labels': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'label': {
+                                'type': 'object',
+                                'properties': {
+                                    'name': {
+                                        'type': 'string'
                                     },
-                                    "add": {
-                                        "type": "array",
-                                        "items": {
-                                            "type": "object",
-                                            "properties": {
-                                                "when": {
-                                                    "type": "object",
-                                                    "properties": {
-                                                        "statuses": {
-                                                            "type": "object"
+                                    'add': {
+                                        'type': 'array',
+                                        'items': {
+                                            'type': 'object',
+                                            'properties': {
+                                                'when': {
+                                                    'type': 'object',
+                                                    'properties': {
+                                                        'statuses': {
+                                                            'type': 'object'
                                                         },
-                                                        "notLabeled": {
-                                                            "type": "array",
-                                                            "items": {
-                                                                "type": "string"
+                                                        'notLabeled': {
+                                                            'type': 'array',
+                                                            'items': {
+                                                                'type': 'string'
                                                             }
                                                         },
-                                                        "canBeMerged": {
-                                                            "type": "boolean"
+                                                        'canBeMerged': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "reviewRequestChange": {
-                                                            "type": "boolean"
+                                                        'reviewRequestChange': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "labeled": {
-                                                            "type": "array",
-                                                            "items": {
-                                                                "type": "string"
+                                                        'labeled': {
+                                                            'type': 'array',
+                                                            'items': {
+                                                                'type': 'string'
                                                             }
                                                         },
-                                                        "reviewResponseToRequestChange": {
-                                                            "type": "boolean"
+                                                        'reviewResponseToRequestChange': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "onPush": {
-                                                            "type": "boolean"
+                                                        'onPush': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "reviewApproved": {
-                                                            "type": "boolean"
+                                                        'reviewApproved': {
+                                                            'type': 'boolean'
                                                         }
                                                     }
                                                 }
                                             },
-                                            "required": [
-                                                "when"
+                                            'required': [
+                                                'when'
                                             ]
                                         }
                                     },
-                                    "remove": {
-                                        "type": "array",
-                                        "items": {
-                                            "type": "object",
-                                            "properties": {
-                                                "when": {
-                                                    "type": "object",
-                                                    "properties": {
-                                                        "statuses": {
-                                                            "type": "object"
+                                    'remove': {
+                                        'type': 'array',
+                                        'items': {
+                                            'type': 'object',
+                                            'properties': {
+                                                'when': {
+                                                    'type': 'object',
+                                                    'properties': {
+                                                        'statuses': {
+                                                            'type': 'object'
                                                         },
-                                                        "notLabeled": {
-                                                            "type": "array",
-                                                            "items": {
-                                                                "type": "string"
+                                                        'notLabeled': {
+                                                            'type': 'array',
+                                                            'items': {
+                                                                'type': 'string'
                                                             }
                                                         },
-                                                        "canBeMerged": {
-                                                            "type": "boolean"
+                                                        'canBeMerged': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "reviewRequestChange": {
-                                                            "type": "boolean"
+                                                        'reviewRequestChange': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "labeled": {
-                                                            "type": "array",
-                                                            "items": {
-                                                                "type": "string"
+                                                        'labeled': {
+                                                            'type': 'array',
+                                                            'items': {
+                                                                'type': 'string'
                                                             }
                                                         },
-                                                        "reviewResponseToRequestChange": {
-                                                            "type": "boolean"
+                                                        'reviewResponseToRequestChange': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "onPush": {
-                                                            "type": "boolean"
+                                                        'onPush': {
+                                                            'type': 'boolean'
                                                         },
-                                                        "reviewApproved": {
-                                                            "type": "boolean"
+                                                        'reviewApproved': {
+                                                            'type': 'boolean'
                                                         }
                                                     }
                                                 }
                                             },
-                                            "required": [
-                                                "when"
+                                            'required': [
+                                                'when'
                                             ]
                                         }
                                     }
                                 },
-                                "required": [
-                                    "name",
-                                    "add",
-                                    "remove"
+                                'required': [
+                                    'name',
+                                    'add',
+                                    'remove'
                                 ]
                             }
                         },
-                        "required": [
-                            "label"
+                        'required': [
+                            'label'
                         ]
                     }
                 }
             },
-            "required": [
-                "labels"
+            'required': [
+                'labels'
             ]
         }
     },
-    "required": [
-        "pulls"
+    'required': [
+        'pulls'
     ]
 } as const;
 
